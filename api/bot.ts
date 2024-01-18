14e0a54bc9fe2d4d5af109f71dcb8ed49b4853bf
proxima812 <mrzuc2015@yandex.ru>
2024-01-18 22:55:41 +0600

    12313131

--- api/bot.ts
+++ api/bot.ts
@@ -1,13 +1,28 @@
-import { Bot, webhookCallback } from "grammy"
+import { Bot, InlineKeyboard, webhookCallback } from "grammy"
 
 const token = process.env.TOKEN
-if (!token) throw new Error("BOT_TOKEN is unset")
+if (!token) throw new Error("TOKEN is unset")
 
 const bot = new Bot(token)
 
-// Handle the /start command.
-bot.command("start", ctx => ctx.reply("Welcome! Up and running."))
-// Handle other messages.
-bot.on("message", ctx => ctx.reply("Got another message!"))
+bot.command("start", ctx => {
+	const keyboard = new InlineKeyboard()
+		.text("Интернет", "internet")
+		.text("Алкоголь", "alcohol")
+
+	ctx.reply("Выберите опцию:", {
+		reply_markup: keyboard,
+	})
+})
+
+bot.callbackQuery("internet", async ctx => {
+	await ctx.answerCallbackQuery()
+	ctx.reply("*Сообщение номер один*")
+})
+
+bot.callbackQuery("alcohol", async ctx => {
+	await ctx.answerCallbackQuery()
+	ctx.reply("~~Сообщение номер два~~")
+})
 
 export default webhookCallback(bot, "https")
