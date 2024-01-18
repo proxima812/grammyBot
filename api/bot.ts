0dd7838dcbfaecd434b5e50e20175ee130d56aa4
proxima812 <mrzuc2015@yandex.ru>
2024-01-18 22:26:39 +0600

    t,fffnm

--- api/bot.ts
+++ api/bot.ts
@@ -6,8 +6,8 @@ if (!token) throw new Error("BOT_TOKEN is unset")
 const bot = new Bot(token)
 
 // Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", ctx => ctx.reply("Welcome! Up and running."))
 // Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", ctx => ctx.reply("Got another message!"))
 
 export default webhookCallback(bot, "http")
